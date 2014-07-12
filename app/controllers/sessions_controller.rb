2fe88f47f26576abf4eb57cc4079e7eb9ee73667
hoanewton <caohoa86hp@gmail.com>
2014-07-12 10:50:00 -0400

    add messages to login logout actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,17 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       session[:current_user] = user.id
+      flash[:notice] = "You're now logged in!"
       redirect_to users_path
     else
+      flash[:notice] = "Login failed: incorrect username and/or password."
       redirect_to login_path
     end
   end
 
   def destroy
     session[:current_user] = nil
+    flash[:notice] = "You're now logged out!"
     redirect_to login_path
   end
 end
