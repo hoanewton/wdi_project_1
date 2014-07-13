a5ee406fdfaca7e650a920af72709f480664c9fc
hoanewton <caohoa86hp@gmail.com>
2014-07-12 21:14:52 -0400

    refactor autogenerate and add autopost

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,12 +6,20 @@ class Post < ActiveRecord::Base
 	def self.autogenerate
 		url = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=source:("The New York Times")&api-key=84fccf8f4af9a0e6bf8794d10b603032:15:69557943'
 		response = HTTParty.get(URI.escape(url))
-		articles = response['response']['docs']
-		raw_article = articles.sample
+		raw_article = response['response']['docs'].sample
 		article = {}
 		article[:title] = raw_article['headline']['print_headline']
 		article[:content] = raw_article['lead_paragraph']
 		article
+	end
+
+	def self.autopost
+		article = Post.autogenerate
+		Post.create!(
+			title: article[:title],
+			content: article[:content],
+			user_id: User.where(username: "admin").first.id
+			)
 	end
 
 end
